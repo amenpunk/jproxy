44f52bfcd687d6f3b4625903be63ebd6604c6689
amenpunk <ondasycircuitos@gmail.com>
2023-04-26 20:17:13 -0600

    test

--- app/src/main/java/broker/jproxy/App.java
+++ app/src/main/java/broker/jproxy/App.java
@@ -9,7 +9,8 @@ public class App {
 
   public static void main(String[] args)  throws Exception {
     // consumir el servicio WSSecurityUsernameService en el metodo stockQuoteRequest
-    URL url = new URL("https://brokergt.pagekite.me/WSSecurityUsernameCodeService/WSSecurityUsernameService.asmx?WSDL");
+    // URL url = new URL("https://brokergt.pagekite.me/WSSecurityUsernameCodeService/WSSecurityUsernameService.asmx?WSDL");
+    URL url = new URL("http://brokergt.pagekite.me/WSSecurityUsernameCode/WSSecurityUsernameService.asmx");
     WSSecurityUsernameService security_service = new WSSecurityUsernameService(url);
     WSSecurityUsernameServiceSoap soap = security_service.getWSSecurityUsernameServiceSoap();
     ArrayOfString a = new ArrayOfString();
